3bcc37aeab8089c5f8ce305c022bbee2f5ddfd5e
dazmadevme <53595854+dazmadevme@users.noreply.github.com>
2019-12-04 05:59:34 -0800

    Update sketch_nov24a.ino

--- sketch_nov24a.ino
+++ sketch_nov24a.ino
@@ -6,11 +6,10 @@
 
 char* password ="1234"; 
 int count = 0; 
-//int relay_pin = 13 ; //pin 13 for relay + solenoid
+int relay_pin = 13; //pin 13 for relay + solenoid
 const int buzzer = 12;
 const byte rows = 4; 
 const byte cols = 4;
-int fingerprintID = 0;
 SoftwareSerial mySerial(2,3);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 char keyMap [rows] [cols] = { 
@@ -20,8 +19,8 @@ char keyMap [rows] [cols] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-byte rowPins [rows] = {4, 5, 6, 7}; 
-byte colPins [cols] = {8, 9, 10, 11};
+byte rowPins [rows] = {11,10,9,8}; 
+byte colPins [cols] = {7,6,5,4};
 Keypad myKeypad = Keypad( makeKeymap(keyMap), rowPins, colPins, rows, cols);
 LiquidCrystal_I2C lcd (0x27, 16, 2); 
 
@@ -29,45 +28,55 @@ void setup(){
   lcd.init();
   lcd.backlight();
   lcd.begin(16, 2);
-  finger.begin(57600);
   pinMode(relay_pin , OUTPUT);
-  
+  finger.begin(57600);
+  if(finger.verifyPassword()){
+    lcd.setCursor(0,0);
+    lcd.print("Fingerprint");
+    lcd.setCursor(0,1);
+    lcd.print("Sensor connected");
+    delay(3000);
+    lcd.clear();
+  }
+  else{
+     lcd.setCursor(0,0);
+     lcd.print("No sensor");
+     lcd.setCursor(0,1);
+     lcd.print("found");
+     delay(3000);
+     lcd.clear();
+     lcd.setCursor(0,0);
+     lcd.print("Check pins");
+
+     while(1){
+      delay(1);
+     }
+  } 
 }
 void loop(){
   int i;
-  char keypressed = myKeypad.getKey();
-  fingerprintID = getFingerprintIDez();
-  
+  char keypressed = myKeypad.getKey(); 
   if (int(keypressed) != 0) {
     lcd.setCursor(0,0);
-    lcd.print("    ");
+    lcd.print("Enter password: ");
     lcd.setCursor(0,1);    
-    for (i=0; i<=count; ++i){
+    for (i=0; i<=count; ++i){ 
       lcd.print("*");
     }    
     if (keypressed == password[count]){ //iteration of continuous typing
         ++count;
         if (count == 4)
         {
-          lcd.setCursor(0,0);
-          lcd.print("Enter password:");
           unlockDoor();                                 
           count = 0;
-        }
+        }       
       } else {
        invalidCode();   
         count = 0;
       }
-   }
-   if (fingerprintID == 1){
-     openDoor();
-     delay(10000);
-     closeDoor();
-   }
-   else {
-     closeDoor();
-  }
+   }  
 }
+
   void invalidCode()  //wrong input of code
 {
   lcd.clear();
@@ -93,17 +102,10 @@ void loop(){
   delay(5000);
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Prompt finger");
-  lcd.setCursor(0,1);
-  lcd.print("print scanner");
-  delay(5000);
-  //add any code to unlock the door here
-   getFingerprintIDez();
-  // delay(100);
-   /**/
-
-  
-} 
+  lcd.print("Place finger");
+  getFingerprintIDez();
+  delay(5000);  
+}
  void displayCodeEntryScreen()  //display the code on the screen
 {
   clearScreen();
@@ -119,44 +121,63 @@ void loop(){
   lcd.setCursor(0,1);
   lcd.print("                    ");
 } 
- 
+
 // returns -1 if failed, otherwise returns ID #
-int getFingerprintIDez() {
+  int getFingerprintIDez() {  
   uint8_t p = finger.getImage();
-  if (p != FINGERPRINT_OK)  return -1;
+  if (p != FINGERPRINT_OK){
+    lcd.setCursor(0,0);
+    lcd.print("Waiting for");
+    lcd.setCursor(0,1);
+    lcd.print("valid finger");
+    delay(3000);
+    lcd.clear();
+    return -1;
+  }
 
   p = finger.image2Tz();
-  if (p != FINGERPRINT_OK)  return -1;
+  if (p != FINGERPRINT_OK){
+    lcd.clear();
+    tone(buzzer, 1000); 
+    delay(1000);        
+    noTone(buzzer);
+    delay(2000);
+    lcd.setCursor(0,0);
+    lcd.print("Messy image");
+    lcd.setCursor(0,1);
+    lcd.print("try again");
+    delay(3000);
+    lcd.clear(); 
+    return -1;
+  }
 
   p = finger.fingerFastSearch();
   if (p != FINGERPRINT_OK){
     lcd.clear();
+    tone(buzzer, 1000); 
+    delay(1000);        
+    noTone(buzzer);
+    delay(2000);
     lcd.setCursor(0,0);
-    lcd.print("Access denied");
+    lcd.print("Invalid finger");
     delay(3000);
-    lcd.clear();
-    lcd.setCursor(0,1);
-    lcd.print("Place finger");    
+    lcd.clear();   
     return -1;
   }
-  // found a match!
+ // found a match!
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Found ID#"); 
   lcd.print(finger.fingerID);
-  lcd.setCursor(0,1); 
-  lcd.print("with confidence of"); 
-  lcd.print(finger.confidence);
-  openDoor();
-  return finger.fingerID; 
-}
-void openDoor(){
-   digitalWrite( relay_pin , HIGH);
-   delay(5000);
-}
-void closeDoor(){
-  digitalWrite(relay_pin , LOW);
   delay(5000);
+  lcd.clear(); 
+  digitalWrite(relay_pin, LOW);
+  delay(10000);
+  digitalWrite(relay_pin , HIGH);
+  delay(5000);
+  return finger.fingerID; 
+  delay(5000);
+     
 }
-}
+
   
